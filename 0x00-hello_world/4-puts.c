2722bc16e65b2bcb1397f463ad29c041ef95852f
dericksheldrick <sheldrickderick9@gmail.com>
2023-06-16 13:03:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - start of the code/entry point
+*return : always return 0 when successful
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
