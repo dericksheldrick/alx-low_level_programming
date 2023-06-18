82dcb1a53d0a46b8a9632cbbd09a78ce99c39c96
dericksheldrick <sheldrickderick9@gmail.com>
2023-06-17 19:48:07 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * all possible combination of two digits
+ * return zero (succees)
+ */
+int main(void)
+{
+	int r;
+	int y;
+
+	for (r = 48; r <= 57; r++)
+
+	for (y = r + 1; y <= 57; y++)
+	{
+		if (r != y)
+		{
+			putchar(r);
+			putchar(y);
+		}
+		if (r == 1 && y == 0)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
