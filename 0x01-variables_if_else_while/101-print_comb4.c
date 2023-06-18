5ced692cb38284a99c84b9a57ba0b99486d45fc2
dericksheldrick <sheldrickderick9@gmail.com>
2023-06-17 20:38:59 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * all possible combination of two digits
+ * return zero (succees)
+ */
+int main(void)
+{
+	int r;
+	int y;
+	int f;
+
+	for (r = 48; r <= 57; r++)
+
+	for (y = r + 1; y <= 57; y++)
+
+	for (f = y + 1; f <= 57; f++)
+	{
+		if ((r != y) != f)
+		{
+			putchar(r);
+			putchar(y);
+			putchar(f);
+		}
+		if (r == 55 && y == 56)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
