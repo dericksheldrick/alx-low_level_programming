17b5ae2c895ab6304fc13ba9fe1b008f4dd86392
dericksheldrick <sheldrickderick9@gmail.com>
2023-06-19 02:30:31 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 # include <stdio.h>
 /**
- *main -  prints exactly with proper grammar, but the outcome is a piece of art
- *return :0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
