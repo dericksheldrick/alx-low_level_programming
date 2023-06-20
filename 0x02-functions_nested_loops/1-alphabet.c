be806258bf34016b70336f1d09877e437ff9f4b2
dericksheldrick <sheldrickderick9@gmail.com>
2023-06-20 09:06:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: 0 sucess.
+ */
+void print_alphabet(void)
+{
+	int m;
+
+	for (m = 97; m <= 122;  m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
