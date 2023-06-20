6b6ce3d7379e22bb92e90b3b28fa74b75cec8b63
dericksheldrick <sheldrickderick9@gmail.com>
2023-06-20 10:09:28 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: character in ASII CODE 
+ * Returns: 1 for lowercase character 0 rest
+ */
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
