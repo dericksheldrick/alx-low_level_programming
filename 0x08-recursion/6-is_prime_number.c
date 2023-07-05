98e4302fad6127b594d1cf1634379ec574a55746
dericksheldrick <sheldrickderick9@gmail.com>
2023-07-05 17:22:35 +0000

    number 6&7

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include"main.h"
+int show_prime(int n, int i);
+/**
+ * is_prime_number - return prime numbers
+ * @n: input the integer
+ * Return: 1- prime number 0- otherwise
+ */
+int is_prime_number(int n)
+{
+	return (show_prime(n, 1));
+}
+/**
+ * show_prime - show prime numbers
+ * @n input the intergers
+ * @i: iterate n
+ * Return: 1-prime number 0-otherwise
+ */
+int show_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	if (n % i == 0 && i > 1)
+		return (0);
+	if ((n / i) < i)
+		return (1);
+	return (show_prime(n, i + 1));
+}
